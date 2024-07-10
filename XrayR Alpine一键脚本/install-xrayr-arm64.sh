76beabffbdbf66bc0d1ece19f1c543d0dabcf102
66622a <71886213+66622a@users.noreply.github.com>
2024-07-10 09:49:40 +0800

    Create install-xrayr-arm64.sh

---
+++ XrayR Alpine一键脚本/install-xrayr-arm64.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# 更新软件源
+apk update
+# 安装依赖项
+apk add wget unzip openrc curl jq
+
+# 获取最新版本号
+LATEST_VERSION=$(curl -s https://api.github.com/repos/XrayR-project/XrayR/releases/latest | jq -r .tag_name)
+
+# 下载 XrayR
+wget https://github.com/XrayR-project/XrayR/releases/download/${LATEST_VERSION}/XrayR-linux-arm64-v8a.zip -O /tmp/XrayR-linux-arm64-v8a.zip
+
+# 解压缩
+unzip /tmp/XrayR-linux-arm64-v8a.zip -d /etc/XrayR
+
+# 添加执行权限
+chmod +x /etc/XrayR/XrayR
+
+# 创建软链接
+ln -s /etc/XrayR/XrayR /usr/bin/XrayR
+
+# 创建 XrayR 服务文件
+cat > /etc/init.d/XrayR <<EOF
+#!/sbin/openrc-run
+
+depend() {
+    need net
+}
+
+start() {
+    ebegin "Starting XrayR"
+    start-stop-daemon --start --exec /usr/bin/XrayR -- -config /etc/XrayR/config.yml
+    eend $?
+}
+
+stop() {
+    ebegin "Stopping XrayR"
+    start-stop-daemon --stop --exec /usr/bin/XrayR
+    eend $?
+}
+
+restart() {
+    ebegin "Restarting XrayR"
+    start-stop-daemon --stop --exec /usr/bin/XrayR
+    sleep 1
+    start-stop-daemon --start --exec /usr/bin/XrayR -- -config /etc/XrayR/config.yml
+    eend $?
+}
+EOF
+
+# 添加执行权限
+chmod +x /etc/init.d/XrayR
+
+# 添加到开机启动项中
+rc-update add XrayR default
+
+echo "安装完成！"
